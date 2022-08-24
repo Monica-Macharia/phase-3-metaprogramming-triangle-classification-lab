8a4bfcb13d79d25d4b54f6680c16bb4d61b50096
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-08-24 08:25:12 +0300

    commit tree 3f481a68a9e760640e5d38400554eb95fd125ec4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+ attr_reader :base, :height, :hypotenuse
+
+ def initialize(base, height, hypotenuse)
+  @base = base
+  @height = height
+  @hypotenuse = hypotenuse
+ end
+ def kind
+     valid
+  if base == height &&  height == hypotenuse
+    :equilateral
+ 
+    elsif base == height || height == hypotenuse || base == hypotenuse
+      :isosceles
+ 
+    else 
+      :scalene
+    end
+ end 
+  def zero_mark
+    [base, height, hypotenuse].all?(&:positive)
+  end
+  def inequality
+    base + height> hypotenuse && base + hypotenuse > height && height + hypotenuse > base
+  end
+  def valid
+    raise TriangleError
+    unless zero_mark && inequality
+  end
+  
+  class TriangleError < StandardError
+  end
 end
